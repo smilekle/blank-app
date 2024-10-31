93c16d3eed24602f5c68eeacdea9e4c421cdfc78
smilekle <smile0102@snu.ac.kr>
2024-10-31 10:38:41 +0000

    첫 웹사이트 개꿀

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("👋🏻 Streamlit 앱 만들기)
+st.subheader("예비교원 워크숍 10월 특강 실습 페이지")
